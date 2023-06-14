f1b4550c7c6ad664f303e49a42e46193ca031027
Furkan <muhammedcengiz1@gmail.com>
2023-06-14 23:54:48 +0300

    add: initial next-js-101

---
+++ ReCoded/Next.js/next.js-101/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>
+        Hello World
+      </h1>
+    </main>
+  )
+}
